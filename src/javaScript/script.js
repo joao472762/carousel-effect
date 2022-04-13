97a1ae5f5c9d42440e9e6e0ab92ae991e7992c6c
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-04-13 15:24:38 -0300

    handle arrrows

--- src/javaScript/script.js
+++ src/javaScript/script.js
@@ -3,25 +3,7 @@ let handleCarosel = {
     actualyWidht: carousel.offsetWidth,
     
     
-    Aroow:{
-        ShowleftArrow(){
-            let {actualyWidht,carouselTotalWidth,carouselVisibleWidth} = handleCarosel
-            if(actualyWidht > carouselVisibleWidth()){
-                let leftArrow = document.getElementById('leftArrow')
-                leftArrow.style.opacity = 1
-                
-
-            }
-            else{
-                let leftArrow = document.getElementById('leftArrow')
-                leftArrow.style.opacity = 0
-
-            }
-            console.log(`actualyWidht = ${actualyWidht}
-            \n visible = ${carouselVisibleWidth()}`)
-        }
-    },
-    
+   
     
     carouselVisibleWidth(){
 
@@ -36,23 +18,87 @@ let handleCarosel = {
     
 
     scrollToRight(){
+        if(this.actualyWidht <= 20){
+            this.actualyWidht = this.carouselVisibleWidth()
+        }
         this.actualyWidht += this.carouselVisibleWidth()
         handleCarosel.carousel.scrollLeft += this.carouselVisibleWidth()
+        handleArrow.exucute()
 
-        handleCarosel.Aroow.ShowleftArrow()
+        
      
         
        
       
     },
     scrollToLeft(){
+       
         this.actualyWidht -= this.carouselVisibleWidth()
         handleCarosel.carousel.scrollLeft -= this.carouselVisibleWidth()
-        handleCarosel.Aroow.ShowleftArrow()
-       
+        handleArrow.exucute()
+      
     },
     
 }
+let handleArrow = {
+    count:0,
+        
+    leftArrow(){
+        let {actualyWidht,carouselTotalWidth,carouselVisibleWidth} = handleCarosel
+       
+        if(actualyWidht > carouselVisibleWidth()){
+            let leftArrow = document.getElementById('leftArrow')
+            leftArrow.style.opacity = 1
+            
+
+        }
+        else{
+            let leftArrow = document.getElementById('leftArrow')
+            leftArrow.style.opacity = 0
+
+        }
+        
+        
+    },
+    rightArrow(){
+        let {actualyWidht, carouselTotalWidth, carouselVisibleWidth} = handleCarosel
+        if(actualyWidht >= carouselTotalWidth()){
+            let rightArrow = document.getElementById('rightArrow');
+            rightArrow.style.opacity = 0
+        }
+        else{
+            let rightArrow = document.getElementById('rightArrow');
+            rightArrow.style.opacity = 1   
+        }
+    },
+    exucute(){
+        let {actualyWidht, carouselTotalWidth, carouselVisibleWidth} = handleCarosel
+        if(actualyWidht < 0){
+            actualyWidht = carouselVisibleWidth()
+        }
+        
+        if(actualyWidht > carouselTotalWidth()){
+            actualyWidht = 0
+            actualyWidht= 2500
+            
+            
+            
+        }
+        
+        try {
+            this.rightArrow()
+            this.leftArrow()
+            
+        } catch (error) {
+            console.error(error)
+            
+        }
+        console.log(`actualyWidht = ${actualyWidht}
+        visible = ${carouselVisibleWidth()}
+        totalWidht${carouselTotalWidth()}`)
+    }
+}
+
 
 
 
